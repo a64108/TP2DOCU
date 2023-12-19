f0d662784c2aa6d0e1f21a801d3a030bb74903c0
André Vieira <a64108@ualg.pt>
2023-12-19 23:12:30 +0000

    logo mudado

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'Oficina Winrar',
   tagline: 'Zipamos qualquer serviço num instante!',
-  favicon: 'img/favicon.ico',
+  favicon: 'img/favicon.png',
 
   // Set the production url of your site here
   url: 'https://github.com',
