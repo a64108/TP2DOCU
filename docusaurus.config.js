66dab2572b74ce45d55abc0445be2d2036f43131
André Vieira <a64108@ualg.pt>
2023-12-19 15:29:29 +0000

    oficina no footer

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -93,11 +93,11 @@ const config = {
         style: 'dark',
         links: [
           {
-            title: 'Docs',
+            title: 'Serviços:',
             items: [
               {
-                label: 'Tutorial',
-                to: '/docs/intro',
+                label: 'Oficina',
+                to: '/pages/oficina',
               },
             ],
           },
@@ -132,7 +132,7 @@ const config = {
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} ADC Projeto. Built with Docusaurus.`,
       },
       prism: {
         theme: lightCodeTheme,
