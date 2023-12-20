ceb67bd38e6d6808f1b86a4366d7ac7474304faa
André Vieira <a64108@ualg.pt>
2023-12-20 17:16:13 +0000

    pris renderer removed

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,10 +1,8 @@
 // @ts-check
-// `@type` JSDoc annotations allow editor autocompletion and type checking
-// (when paired with `@ts-check`).
-// There are various equivalent ways to declare your Docusaurus config.
-// See: https://docusaurus.io/docs/api/docusaurus-config
+// Note: type annotations allow type checking and IDEs autocompletion
 
-import {themes as prismThemes} from 'prism-react-renderer';
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
@@ -87,6 +85,7 @@ const config = {
           },
         ],
       },
+      
       footer: {
         style: 'dark',
         links: [
